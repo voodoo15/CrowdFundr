4df74ded4efde6228d30d640e94e69ec2fb96d75
michaelkthai <mthai27@gmail.com>
2016-05-03 18:46:50 -0400

    Created users controller and updated new and create actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to projects_url, notice: "Successfully signed up!"
+    else
+      render "new"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password_digest, :first_name, :last_name,
+                                 :address, :city, :province, :postal_code)
   end
 end
