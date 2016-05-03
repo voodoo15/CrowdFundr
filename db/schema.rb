3836ddf703b78f728701cde1074617e57b674e66
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-05-03 17:58:06 -0400

    Created category model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503215556) do
+ActiveRecord::Schema.define(version: 20160503215737) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "pledges", force: :cascade do |t|
     t.float    "amount"
