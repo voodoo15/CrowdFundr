3236977ddc62e7feff9ace5dfc6cce03f7dfaa3f
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-05-04 15:37:03 -0400

    Added Sorcery email validation on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ActiveRecord::Base
   has_many :projects
   has_many :pledges
   has_many :rewards, through: :pledges
+
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+
+  validates :email, uniqueness: true
 end
