92b819d5129d39710eeabd35de86fee479d3d391
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-05-04 19:39:08 -0400

    Updated project methods for backer count, amount pledged and project ended.  Have to update days remaining

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,19 @@ class Project < ActiveRecord::Base
   belongs_to :category
   has_many :rewards
   has_many :pledges, through: :rewards
+
+  def count_backers
+    return self.pledges.count
+  end
+
+  def amount_pledged
+    return self.pledges.sum(:amount)
+  end
+
+  def ended?
+    DateTime.now.to_date > self.end_date
+  end
+
+  def days_remaining
+  end
 end
