7a26a057ff658375767e2489866436c4816ba986
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-05-04 16:42:39 -0400

    Added routes for sorcery, turned off reset password module and added nav to application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :user_sessions
+  resources :users
 
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
