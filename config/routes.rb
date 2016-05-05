e04816929a5b661cb772d1f5e31b58643d5bb749
michaelkthai <mthai27@gmail.com>
2016-05-05 18:21:43 -0400

    created pledges routes and started main project info table for show page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :user_sessions
   resources :users
   resources :projects, only: [:index, :show, :new, :create, :edit]
+  resources :pledges
+
 
   get 'login' => 'user_sessions#new', :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
