d5c6827ba408b9aeea8f6f60fabe9168312a9f55
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-05-05 03:40:04 -0400

    Turned on mailer to check if it was actually working in the first place

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,15 +43,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = false #Change this to send deliveries
+  config.action_mailer.perform_deliveries = true #Change this to send deliveries
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            Rails.application.secrets.smtp_gmail_username,
-    password:             Rails.application.secrets.smtp_gmail_password,
+    user_name:            Rails.application.secrets.gmail_username,
+    password:             Rails.application.secrets.gmail_password,
     authentication:       'plain',
     enable_starttls_auto: true  }
 end
