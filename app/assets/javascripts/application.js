48e21eb2ac1839c8a9ee6488709b731d97735db3
michaelkthai <mthai27@gmail.com>
2016-05-06 00:52:43 -0400

    Structured/styled show and index pages more, added parallax to index, added pledge buttons on show, placeholder videos. Working on a new navbar.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,67 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+// NAVBAR
+$(window).on("load resize",function(e) {
+  var more = document.getElementById("js-centered-more");
+
+  if ($(more).length > 0) {
+    var windowWidth = $(window).width();
+    var moreLeftSideToPageLeftSide = $(more).offset().left;
+    var moreLeftSideToPageRightSide = windowWidth - moreLeftSideToPageLeftSide;
+
+    if (moreLeftSideToPageRightSide < 330) {
+      $("#js-centered-more .submenu .submenu").removeClass("fly-out-right");
+      $("#js-centered-more .submenu .submenu").addClass("fly-out-left");
+    }
+
+    if (moreLeftSideToPageRightSide > 330) {
+      $("#js-centered-more .submenu .submenu").removeClass("fly-out-left");
+      $("#js-centered-more .submenu .submenu").addClass("fly-out-right");
+    }
+  }
+
+  var menuToggle = $("#js-centered-navigation-mobile-menu").unbind();
+  $("#js-centered-navigation-menu").removeClass("show");
+
+  menuToggle.on("click", function(e) {
+    e.preventDefault();
+    $("#js-centered-navigation-menu").slideToggle(function(){
+      if($("#js-centered-navigation-menu").is(":hidden")) {
+        $("#js-centered-navigation-menu").removeAttr("style");
+      }
+    });
+  });
+}); 
+// PARALLAX
+$(document).ready(function() {
+  if ($("#js-parallax-window").length) {
+    parallax();
+  }
+});
+
+$(window).scroll(function(e) {
+  if ($("#js-parallax-window").length) {
+    parallax();
+  }
+});
+
+function parallax(){
+  if( $("#js-parallax-window").length > 0 ) {
+    var plxBackground = $("#js-parallax-background");
+    var plxWindow = $("#js-parallax-window");
+
+    var plxWindowTopToPageTop = $(plxWindow).offset().top;
+    var windowTopToPageTop = $(window).scrollTop();
+    var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
+
+    var plxBackgroundTopToPageTop = $(plxBackground).offset().top;
+    var windowInnerHeight = window.innerHeight;
+    var plxBackgroundTopToWindowTop = plxBackgroundTopToPageTop - windowTopToPageTop;
+    var plxBackgroundTopToWindowBottom = windowInnerHeight - plxBackgroundTopToWindowTop;
+    var plxSpeed = 0.35;
+
+    plxBackground.css('top', - (plxWindowTopToWindowTop * plxSpeed) + 'px');
+  }
+}
