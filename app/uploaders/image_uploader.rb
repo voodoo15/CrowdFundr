d0046b3420f9dbf4c21c207e9a42ea642f10dd99
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2016-05-07 20:15:20 -0400

    Added funded method in project model to find out if the project is funded

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -17,7 +17,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url
+  def default_url(*args)
     # For Rails 3.1+ asset pipeline compatibility:
     # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
 
