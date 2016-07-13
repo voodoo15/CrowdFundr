ac0c504a66a6c03855afa47819258e6ebdb9e791
michaelkthai <mthai27@gmail.com>
2016-07-13 15:09:20 -0400

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,58 +31,66 @@ categories.each do |category|
   Category.create!( description:  category )
 end
 
-#   User.create!(
-#     email:  "mike.thai@gmail.com",
-#     first_name: "Mike",
-#     last_name:  "Thai",
-#     address:  "123 fake street",
-#     city:  "Toronto",
-#     province:  "ON",
-#     postal_code:  "M4K2E5",
-#     password:  "Mike",
-#     password_confirmation:  "Mike",
-#     phone_number:  1234567891
-#   )
-#
-  Project.create!(
-    title:  "Iphone",
-    short_description:  "Nothin but the best",
-    long_description:  "The iPhone 6 and iPhone 6 Plus are smartphones designed, and marketed by Apple Inc. The devices are part of the iPhone series and were announced on September 9, 2014, and released on September 19, 2014.[16] The iPhone 6 and iPhone 6 Plus jointly serve as successors to the iPhone 5c and iPhone 5s.  The iPhone 6 and iPhone 6 Plus include a number of changes over their predecessor, including models with larger 4.7 and 5.5 inches (120 and 140 mm) displays, a faster processor, upgraded cameras, improved LTE and Wi-Fi connectivity and support for a near field communications-based mobile payments offering",
-    risk_description:  "Possibility of damage to the shipped games and possible slippage of delivery date",
-    end_date:  DateTime.tomorrow,
-    category_id:  2,
-    funding_amount:  50000,
-    user_id:  1
+  User.create!(
+    email:  "mike.thai@gmail.com",
+    first_name: "Mike",
+    last_name:  "Thai",
+    address:  "123 fake street",
+    city:  "Toronto",
+    province:  "ON",
+    postal_code:  "M4K2E5",
+    password:  "Mike",
+    password_confirmation:  "Mike",
+    phone_number:  1234567891
   )
 #
-# Reward.create!(
-#   title:  "Reward 1",
-#   description:  "Get 50% If you order before 2017-05-07",
-#   amount:  10.0,
-#   project_id:  1
-# )
-#
-# Reward.create!(
-#   title:  "Reward 2",
-#   description:  "Be the first to receive the first Iphone 6 on 2017-08-04",
-#   amount:  500.0,
-#   project_id:  1
-# )
-#
-# Pledge.create!(
-#   amount:  10.0,
-#   user_id:  1,
-#   reward_id:  1
-# )
-#
-# Pledge.create!(
-#   amount:  500.0,
-#   user_id:  1,
-#   reward_id:  2
-# )
-#
-# Pledge.create!(
-#   amount:  10.0,
-#   user_id:  2,
-#   reward_id:  1
-# )
+  Project.create!(
+    title:  "Help fund my record!",
+    short_description:  "My recording studio got robbed and now all my gear is gone. Please send me some $ so I can buy new gear and finish my record!",
+    long_description:  "Don't you hate when people break into your place and steal all of your most expensive things and material you've been working on for many years? Me too. That's why you should send me some contributions so I can replace all of my stolen items and then treat myself on a nice vacation so that I can forget the terrible injustice that has been done to me. In fact, I might just spend all the money on my vacation and maybe I'll be inspired to write totally new material to share with you. It's a win-win, right?? ",
+    risk_description:  "Possibility of damage to the shipped games and possible slippage of delivery date",
+    end_date:  DateTime.tomorrow,
+    category_id:  12,
+    funding_amount:  150000,
+    user_id:  1,
+    image: "http://i.ytimg.com/vi/Q4StEidEyBs/maxresdefault.jpg",
+  )
+
+Reward.create!(
+  title:  "Be first to receive the new record",
+  description:  "For only a 10 dollar contribution, be one of the first to receive a vinyl pressing of my record when it comes out! Vinyls come with an unlimited free download code and unique, handdrawn album art by yours truly.",
+  amount:  10.0,
+  project_id:  1
+)
+
+Reward.create!(
+  title:  "Receive 1st pressing of record and an invitation to my CD launch party",
+  description:  "For $100, you will receive the first reward, along with an exclusive VIP invitation to the record launch party at Site Sound Studios in downtown Toronto. There will be free drinks and food and a surprise performance by a surprise artist.",
+  amount:  100.0,
+  project_id:  1
+)
+
+Reward.create!(
+  title:  "A private set for you and your friends",
+  description:  "For $2000, you will receive the first reward, a VIP invitation to the record launch party, AND my band and I will fly out to whereever you are in Canada or the US to play a personal performance just for you.",
+  amount:  2000.0,
+  project_id:  1
+)
+
+Pledge.create!(
+  amount:  10.0,
+  user_id:  1,
+  reward_id:  1
+)
+
+Pledge.create!(
+  amount:  100.0,
+  user_id:  1,
+  reward_id:  2
+)
+
+Pledge.create!(
+  amount:  1500.0,
+  user_id:  1,
+  reward_id:  3
+)
